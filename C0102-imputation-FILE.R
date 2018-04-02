71de4513c0a0688b680d11b8505c5f476ba7585e
hunyong.cho <hunyong.cho@gmail.com>
2018-04-02 15:53:12 -0400

    ..

--- C0102-imputation-FILE.R
+++ C0102-imputation-FILE.R
@@ -6,8 +6,13 @@ library(mice)
   m <- 25 # number of imputations
   set.seed(100)
   a <- Sys.time()
-  data.FILE.imp <- mice(data.working[,tmp$variable %>% as.character], m=m)
-  data.FILE.list <- lapply(1:m, function(i) complete(data.FILE.imp, action = i))
+  data.mice.FILE.imp <- mice(data.working[,tmp$variable %>% as.character], m=m)
+  data.mice.FILE.list <- lapply(1:m, function(i) complete(data.mice.FILE.imp, action = i))
   Sys.time() - a #18.13 hours
-  saveRDS(data.FILE.imp, "../dataImputed/data.FILE.imp.rds")
-  saveRDS(data.FILE.list, "../dataImputed/data.FILE.list.rds")
+  saveRDS(data.mice.FILE.imp, "../dataImputed/data.mice.FILE.imp.rds")
+  saveRDS(data.mice.FILE.list, "../dataImputed/data.mice.FILE.list.rds")
+
+# 2.0 summary of FILE
+  tmp.FILE <- summarizedVar(data = data.mice.FILE.list, var.name.matrix = FILE, measure = sum, var.name = "FILE")
+  
+  
