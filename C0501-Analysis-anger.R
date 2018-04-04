bc4ece90f5d074b55e4eee20abb337392733ac7b
hunyong.cho <hunyong.cho@gmail.com>
2018-04-04 14:50:08 -0400

    spaghetti plot added

--- C0501-Analysis-anger.R
+++ C0501-Analysis-anger.R
@@ -13,6 +13,7 @@ library(lme4)
 library(dplyr)
 library(magrittr)
 library(geepack)
+library(ggplot2)
 
 ## 0. getting summary measures and long formats
   # Getting summary measures
@@ -25,41 +26,47 @@ library(geepack)
   # transform time(1,2,3,4,5) to months
   data.working.base.anger.long %<>% visit2month
   
+  # change (trt = 1,2) to (trt = 0,1)
+  data.working.base.anger.long %<>% trt01
+  data.working.base.anger %<>% trt01
+  
+  
 #+ warning=FALSE
 #' ## 1. t-test  
-  lm(unexp.3 - unexp.1 ~ treatmgroup_nr, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
-  lm(extexp.3 - extexp.1 ~ treatmgroup_nr, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
-  lm(emoreg.3 - emoreg.1 ~ treatmgroup_nr, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
-  lm(probsol.3 - probsol.1 ~ treatmgroup_nr, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
+  lm(unexp.3 - unexp.1 ~ trt, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
+  lm(extexp.3 - extexp.1 ~ trt, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
+  lm(emoreg.3 - emoreg.1 ~ trt, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
+  lm(probsol.3 - probsol.1 ~ trt, data = data.working.base.anger[[1]]) %>% summary %>% "["("coefficients")
 
 #' <br>
 #' <br>
 #' 
 #+ warning=FALSE
 #' ## 2. spaghetti plots
-
+  data.working.base.anger.long[[1]] %>%
+    ggplot(aes(month, unexp, col = factor(id), shape=id)) + geom_line()
   
   
 #' <br>
 #' <br>
 #' 
 #' ## 3. mixed effect models  
-  lmer(unexp ~ treatmgroup_nr * month  + (treatmgroup_nr|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  lmer(extexp ~ treatmgroup_nr * month + (treatmgroup_nr|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  lmer(emoreg ~ treatmgroup_nr * month + (treatmgroup_nr|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  lmer(probsol ~ treatmgroup_nr * month + (treatmgroup_nr|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(unexp ~ trt * month  + (trt|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(extexp ~ trt * month + (trt|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(emoreg ~ trt * month + (trt|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(probsol ~ trt * month + (trt|id), data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
   
 
 #' <br>
 #' <br>
 #' 
 #' ## 4. marginal models (GEE)
-  geeglm(unexp ~ treatmgroup_nr * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  geeglm(extexp ~ treatmgroup_nr * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  geeglm(emoreg ~ treatmgroup_nr * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  geeglm(probsol ~ treatmgroup_nr * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(unexp ~ trt * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(extexp ~ trt * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(emoreg ~ trt * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(probsol ~ trt * month, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
   
-  geeglm(IBSS ~ treatmgroup_nr * month + unexp*treatmgroup_nr, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
-  geeglm(IBSS ~ treatmgroup_nr * month + unexp + extexp + emoreg + probsol, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(IBSS ~ trt * month + unexp*trt, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(IBSS ~ trt * month + unexp + extexp + emoreg + probsol, id = id, data = data.working.base.anger.long[[1]]) %>% summary %>% "["("coefficients")
   
   
