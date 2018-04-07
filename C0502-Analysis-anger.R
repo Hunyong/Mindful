9eeb940f9dd8491773a92f788c46042bc0f4430a
hunyong.cho <hunyong.cho@gmail.com>
2018-04-07 00:24:27 -0400

    change file name

---
+++ C0502-Analysis-anger.R
@@ -0,0 +1,76 @@
+#' ---
+#' title: Anger variables. t-test and mixed effect models based on a single dataset (will do multiple set combination later)
+#' author: Group H
+#' date: April 2, 2018
+#' ---
+#+ echo=FALSE, warning=FALSE
+
+### C0501 Analysis - anger
+## 0. library
+source("F0101-variableSet.R")
+source("F0501-timeFunction.R")
+library(lme4)
+library(dplyr)
+library(magrittr)
+library(geepack)
+library(ggplot2)
+
+## 0. getting summary measures and long formats
+  # Getting summary measures
+  data.working.base <- readRDS("../dataImputed/data.working.base.rds")
+  data.working.anger <- readRDS("../dataImputed/data.working.anger.rds")
+  data.working.FILE <- readRDS("../dataImputed/data.working.FILE.rds")
+  data.working.baf <- cbind.list(data.working.base, data.working.anger)
+  data.working.baf <- cbind.list(data.working.baf, data.working.FILE)
+  data.working.baf.long <- long.list (data.list = data.working.baf, 
+                                             long.vars = c("IBSS","unexp","extexp", "emoreg", "probsol"))
+
+  # transform time(1,2,3,4,5) to months
+  data.working.baf.long %<>% visit2month
+  
+  # change (trt = 1,2) to (trt = 0,1)
+  data.working.baf.long %<>% trt01
+  data.working.baf %<>% trt01
+  
+  
+  
+    
+#+ warning=FALSE
+#' ## 1. t-test / ANCOVA  
+  lm(unexp.3 - unexp.1 ~ trt * FILE.base, data = data.working.baf[[1]]) %>% summary %>% "["("coefficients")
+  lm(extexp.3 - extexp.1 ~ trt * FILE.base, data = data.working.baf[[1]]) %>% summary %>% "["("coefficients")
+  lm(emoreg.3 - emoreg.1 ~ trt * FILE.base, data = data.working.baf[[1]]) %>% summary %>% "["("coefficients")
+  lm(probsol.3 - probsol.1 ~ trt * FILE.base, data = data.working.baf[[1]]) %>% summary %>% "["("coefficients")
+
+#' <br>
+#' <br>
+#' 
+#+ warning=FALSE
+#' ## 2. spaghetti plots
+  data.working.baf.long[[1]] %>%
+    ggplot(aes(month, unexp, col = factor(id), shape=id)) + geom_line()
+  
+  
+#' <br>
+#' <br>
+#' 
+#' ## 3. mixed effect models  
+  lmer(unexp ~ trt * month  + (trt|id), data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(extexp ~ trt * month + (trt|id), data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(emoreg ~ trt * month + (trt|id), data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  lmer(probsol ~ trt * month + (trt|id), data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  
+
+#' <br>
+#' <br>
+#' 
+#' ## 4. marginal models (GEE)
+  geeglm(unexp ~ trt * month, id = id, data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(extexp ~ trt * month, id = id, data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(emoreg ~ trt * month, id = id, data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(probsol ~ trt * month, id = id, data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  
+  geeglm(IBSS ~ trt * month + unexp*trt, id = id, data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  geeglm(IBSS ~ trt * month + unexp + extexp + emoreg + probsol, id = id, data = data.working.baf.long[[1]]) %>% summary %>% "["("coefficients")
+  
+  
