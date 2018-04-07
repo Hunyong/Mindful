3371edcc40849f65da4ad27ef8a7e4bbd40c3afa
hunyong.cho <hunyong.cho@gmail.com>
2018-04-07 15:34:20 -0400

    Analysis-anger-ibss added

---
+++ C0502-Analysis-anger-IBSS.R
@@ -0,0 +1,77 @@
+#' ---
+#' title: Anger and IBSS data analysis. t-test and marginal longitudinal models
+#' author: Group H
+#' date: April 9, 2018
+#' ---
+#+ echo=FALSE, warning=FALSE, include=FALSE
+
+## 0. library
+source("F0101-variableSet.R")
+source("F0501-timeFunction.R")
+source("F0502-analysis.R")
+library(lme4); library(geepack)
+library(dplyr); library(magrittr)
+library(ggplot2); library(knitr)
+
+m = 25
+
+## 0. getting summary measures and long formats
+# Getting summary measures
+data.working.base <- readRDS("../dataImputed/data.working.base.rds")
+data.working.anger <- readRDS("../dataImputed/data.working.anger.rds")
+data.working.FILE <- readRDS("../dataImputed/data.working.FILE.rds")
+data.working.baf <- cbind.list(data.working.base, data.working.anger)
+data.working.baf <- cbind.list(data.working.baf, data.working.FILE)
+data.working.baf.long <- long.list (data.list = data.working.baf, 
+                                    long.vars = c("IBSS","unexp","extexp", "emoreg", "probsol"))
+
+# transform time(1,2,3,4,5) to months
+data.working.baf.long %<>% visit2month
+
+# change (trt = 1,2) to (trt = 0,1)
+data.working.baf.long %<>% trt01
+data.working.baf %<>% trt01
+
+
+
+
+
+#' All the analyses in this report are based on 25 multiple imputation, unless otherwise mentioned.
+#' 
+#' ## 1. ANCOVA of 3-month IBSS change and interaction with anger (at baseline)
+#' ### Model. (IBSS.3 - IBSS.1) ~ trt  * (unexp.1 + extexp.1 + emoreg.1 + probsol.1)
+#+ warning=FALSE, echo=FALSE
+
+lapply(1:m, function(i) 
+  lm(IBSS.3 - IBSS.1 ~ trt  * (unexp.1 + extexp.1 + emoreg.1 + probsol.1), data = data.working.baf[[i]]) %>% summary %>% "[["("coefficients")) %>% 
+  Rubin.list() %>% kable(digits=3, caption = "ANCOVA of Unexpressed Anger controlling for distressing events")
+
+#' <br>
+#' <br>
+#' 
+#' ## 2. marginal models (GEE)
+#' ### Model. IBSS ~ trt * month * anger
+#' The Marginal models were fitted with the full-interaction design.
+#' 
+#' <br>
+#' All but the Problem solving model coefficients were non-significant.
+#' 
+#+ echo=FALSE, warning=FALSE
+
+lapply(1:m, function(i) 
+  geeglm(IBSS ~ trt * month * (unexp + extexp + emoreg + probsol), id = id, data = data.working.baf.long[[i]]) %>% summary %>% "[["("coefficients")) %>% 
+  Rubin.list(std.name = "Std.err") %>% kable(digits=3, caption = "Marginal model of IBSS adjusted for anger scores")
+
+
+#' ## Second model - Some interactions removed
+#' ### Model. IBSS ~ trt * month + anger
+#' 
+#' 
+#+ echo=FALSE, warning=FALSE
+
+lapply(1:m, function(i) 
+  geeglm(IBSS ~ trt * month + (unexp + extexp + emoreg + probsol), id = id, data = data.working.baf.long[[i]]) %>% summary %>% "[["("coefficients")) %>% 
+  Rubin.list(std.name = "Std.err") %>% kable(digits=3, caption = "Marginal model of Problem Solving adjusted for FILE score")
+
+#' Overall, people with many distressing events have higher scores
+#' (small p-values for all but emotion regulation).
